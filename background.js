892d86d619e2eb8f1ab4c512be63d321aba798c8
David Anthony Bau <dab1998@gmail.com>
2018-02-06 15:39:38 -0500

    Clean up and make permit mode temporary

--- background.js
+++ background.js
@@ -1,55 +1,130 @@
 // Whitelist in under 100 lines of code.
-var allowed_urls = [];
-var blocked_urls = [];
-var current_mode = 'forbid';
+var allowed_urls = []; // List of whitelist matching functions for URLs
+var current_mode = 'forbid'; // Flag allowing user to temporarily permit all URLs. Can also be "permit"
 
+/*
+ * 1. MAIN ENDPOINT. We bind to the onBeforeRequest listener.
+ */
+chrome.webRequest.onBeforeRequest.addListener((info) => {
+  (info) => {
+    // If we are in "permit" mode, then we allow everything. We also allow anything from
+    // the local filesystem.
+    var is_allowed = (current_mode === 'permit');
 
+    // allowed_urls will be an array of functions that return true or false.
+    // Other than filesystem urls, we allow only urls from this array.
+    is_allowed = is_allowed || allowed_urls.any((x) => {return x(info.url);});
+
+    // If we disallowed a url, record this so that you can see.
+    if (!is_allowed) blocked_urls.push(info.url)
+
+    // Returning 'cancel' cancels the request; do so
+    // if we don't allow this.
+    return {'cancel': !is_allowed}
+  },
+
+  // We need to watch every URL on http or https.
+  {urls: ['http://*/*', 'https://*/*']},
+
+  // We need permission to block requests.
+  ['blocking']
+);
+
+/*
+ * 2. PARSING CODE. We need to know how to: (1) extract domains from urls
+ * and (2) parse the configuration file.
+ */
+
+// 2.1. Extracting a domain from a URL
 function get_domain (url) {
   // This regex matches: {protocol}   {username:password@}?{domain}
   var match = url.match(/^https?:\/\/([^:@\/]*:?[^:@\/]*@)?([a-zA-Z0-9-\.]*)/);
   if (match != null) return match[2];
 }
 
+// 2.2. Parsing the configuration format. This returns an array of
+// functions which return true for matches and false for non-matches.
 function get_allowed_urls (config) {
+
+  // Strip whitespace and comments.
   config = config.split('\n').map(function(x) {
     return x.trim();
   }).filter(function(x) {
     return x.length > 0 && x[0] != '#';
   });
 
+  // Each remaining line is the configuration for one matching
+  // function.
   return config.map(function(x) {
+    // Two kinds of configuartion elements:
+
+    // (a) regex, matching the entire URL except the protocol, which
+    // begin with '/' and follow Javascript Regex syntax.
     if (x[0] == '/') {
-      return (function(url) {
+      return (url) => {
+        // Pad with possible protocols and force matching the entire URL
         return url.match(new RegExp('^https?:\/\/' + x.slice(1) + '([\?#].*)?$')) != null;
-      });
+      };
+
+    // (b) domains; any other kind of line.
     } else {
-      return (function(url) { return get_domain(url) === x; });
+      return (url) => { return get_domain(url) === x; };
     }
+
   });
 }
 
+/*
+ * Load and parse configuration file, and watch for changes to the configuration
+ * file (loading and parsing whenever it changes).
+ */
+
+// Loading exiting config
 chrome.storage.local.get('allowed_urls', function (data) {
   allowed_urls = get_allowed_urls(data['allowed_urls']);
 });
 
+// Watch for changes to the config
 chrome.storage.onChanged.addListener(function (changes, namespace) {
   if (namespace === 'local' && (changes['allowed_urls'] != null))
     allowed_urls = get_allowed_urls(changes['allowed_urls'].newValue);
 });
 
-chrome.webRequest.onBeforeRequest.addListener(
-  function (info) {
-    var is_allowed = (current_mode == 'permit');
-    for (var i = 0; i < allowed_urls.length; i++)
-      is_allowed = is_allowed || allowed_urls[i](info.url);
+/*
+ * SETTING "PERMIT" MODE. Permit mode is controlled by the "current_mode" flag and
+ * can be set to "permit" at one minute increments. TODO: change the icon to something red
+ * when in permit mode as well.
+ */
+unset_permit_timeout = null;
 
-    if (current_mode === 'confirm')
-      is_allowed = is_allowed || confirm('Send a request to: ' + info.url);
+// To go back to forbid mode, both cancel the timeout and also
+// set the flag.
+function unset_permit () {
+  current_mode = 'forbid';
+  if (unset_permit_timeout != null) {
+    clearTimeout(unset_permit_timeout);
+    unset_permit_timeout = null;
+  }
+}
 
-    if (!is_allowed) blocked_urls.push(info.url)
+// To set permit mode, set a timeout to confirm staying in it,
+// and also set the flag.
+function set_permit () {
+  if (unset_permit_timeout != null)
+    clearTimeout(unset_permit_timeout);
 
-    return {'cancel': !is_allowed}
-  },
-  {urls: ['http://*/*', 'https://*/*']},
-  ['blocking']
-);
+  // Set the flag.
+  current_mode = 'permit';
+
+  // Set timeout to only permit for 1 minute at a time.
+  unset_permit_timeout = setTimeout(
+    () => {
+      if (confirm('You are in whitelist permit mode. Continue in permit mode?')) {
+        set_permit();
+      } else {
+        unset_permit();
+      }
+    },
+    1000 * 60
+  );
+}
--- popup.js
+++ popup.js
@@ -1,38 +1,55 @@
+/*
+ * Grab parts of the DOM
+ */
 var textarea = document.getElementById('allowed_urls'),
     update = document.getElementById('update'),
     forbid = document.getElementById('forbid'),
-    confirm_btn = document.getElementById('confirm'),
     permit = document.getElementById('permit'),
     behavior = document.getElementById('behavior'),
     botr = document.getElementById('botr'),
-    background;
-chrome.runtime.getBackgroundPage(function (b) {
+    background; // Also declare this global, which will become a reference to
+                // the background page.
+
+/*
+ * Get a reference to the background page.
+ */
+chrome.runtime.getBackgroundPage((b) => {
   background = b;
+
+  // Update the mode flag display to reflec the background page's
+  // current mode flag.
   behavior.innerText = background.current_mode;
 
-  for (var i = background.blocked_urls.length - 1; i >= 0; i--) {
+  // List out the 50 most recently blocked URLs in divs.
+  background.blocked_urls.slice(-50).forEach((blocked_url) => {
     var new_el = document.createElement('div');
-    new_el.innerText = background.blocked_urls[i];
+    new_el.innerText = blocked_url;
 
     botr.appendChild(new_el);
-  }
+  });
 });
 
-chrome.storage.local.get('allowed_urls', function(data) {
+// Load the current config file.
+chrome.storage.local.get('allowed_urls', (data) => {
   textarea.value = data['allowed_urls'];
 });
-textarea.addEventListener('input', function() {
+
+// Every time the user changes the config file edit input,
+// update the config file.
+textarea.addEventListener('input', () => {
   chrome.storage.local.set({'allowed_urls': textarea.value});
 });
-forbid.addEventListener('click', function() {
-  if (background)
-    behavior.innerText = background.current_mode = 'forbid';
+
+// Add listeners for changing the 'permit mode' flag.
+forbid.addEventListener('click', () => {
+  if (background) {
+    behavior.innerText = 'forbid';
+    background.unset_permit();
+  }
 });
-confirm_btn.addEventListener('click', function() {
-  if (background)
-    behavior.innerText = background.current_mode = 'confirm';
-});
-permit.addEventListener('click', function() {
-  if (background)
-    behavior.innerText = background.current_mode = 'permit';
+permit.addEventListener('click', () => {
+  if (background) {
+    behavior.innerText = 'permit';
+    background.set_permit();
+  }
 });
